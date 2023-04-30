4be272048c2f1ffbfb385bb4cf799468ce69ce9c
Hilsa Parinding <h.parinding@gmail.com>
2023-04-30 14:47:16 -0400

    Add files via upload

---
+++ HelloWorld Program/script.js
@@ -0,0 +1,5 @@
+// Display a "Hello World" alert
+alert("Hello World");
+
+// Set the text of the heading element to "Hello, World!"
+document.getElementById("greeting").innerHTML = "Hello, World!";
